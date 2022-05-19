97f58711789c9ba3fc2bcc8eccbf5f47656d00b8
nocaption722 <brandon.lee722@yahoo.com>
2022-05-18 19:00:28 -0500

    Adding files

--- schema.sql
+++ schema.sql
@@ -1,52 +1,151 @@
--- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
-     PRIMARY KEY (dept_no),
-     UNIQUE (dept_name)
+-- Creating tables for PH-EmployeesDB
+
+CREATE TABLE departments(
+	dept_no VARCHAR (4) NOT NULL,
+	dept_name VARCHAR (40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
 );
 
-CREATE TABLE employees (
-     emp_no INT NOT NULL,
-     birth_date DATE NOT NULL,
-     first_name VARCHAR NOT NULL,
-     last_name VARCHAR NOT NULL,
-     gender VARCHAR NOT NULL,
-     hire_date DATE NOT NULL,
-     PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
-);
-
-CREATE TABLE salaries (
+CREATE TABLE employees(
 	emp_no INT NOT NULL,
-  	salary INT NOT NULL,
-  	from_date DATE NOT NULL,
-  	to_date DATE NOT NULL,
-  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL, 
+	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_emp (
-	dept_no VARCHAR NOT NULL,
+CREATE TABLE dept_emp(
+emp_no INT NOT NULL,
+dept_no VARCHAR(6) NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+CREATE TABLE dept_manager(
+dept_no VARCHAR (4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE titles (
+CREATE TABLE salaries(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL, 
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE titles(
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, from_date)
 );
 
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+
+DROP TABLE dept_emp CASCADE;
+DROP TABLE dept_manager CASCADE;
+
+SELECT * FROM titles;
+SELECT * FROM dept_emp;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name 
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31'
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+DROP TABLE retirement_info;
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+dept_emp.to_date
+INTO current_emp
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no
+WHERE dept_emp.to_date = ('9999-01-01');
+
+SELECT COUNT(current_emp), dept_emp.dept_no
+INTO bobby_file
+FROM current_emp
+LEFT JOIN dept_emp
+ON current_emp.emp_no = dept_emp.emp_no
+GROUP BY dept_emp.dept_no
+ORDER BY dept_emp.dept_no;
